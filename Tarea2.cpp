db5dcfc5430a8ecc8b866cb0a9d2624f8ae1de74
marcolg2404 <marco.garridom@utem.cl>
2019-04-27 12:56:38 -0400

    50% avanzado

--- Tarea2.cpp
+++ Tarea2.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+int arreglo[100000];
+void llenar_arreglo(){
+  srand (time(NULL));
+  for(int i=0; i<=100000;i++){
+      int aleatorio = rand() % 10000+1;
+      arreglo[i]=aleatorio;
+  }
+}
+
+float suma(int arreglo[]){
+  float suma;
+  for(int i=0; i<=100000;i++){
+    suma=suma+(arreglo[i]);
+  }
+  return suma;
+}
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  float prom;
+  llenar_arreglo();
+  prom=(suma(arreglo)/p);
+  cout<<prom<<endl;
+  return 0;
 }
