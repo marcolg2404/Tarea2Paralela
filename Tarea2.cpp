7934781483812a38b5e5a37f86aa57dcb4493216
marcolg2404 <marco.garridom@utem.cl>
2019-04-28 15:24:32 -0400

    Funciones finalizadas con sus comentarios correspondientes.

--- Tarea2.cpp
+++ Tarea2.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <time.h>
+#include <time.h> //Libreria para el seed de numeros aleatorios
 #include <stdlib.h>
+#include <cmath> // Libreria matematica
 
-int arreglo[100000];
-void llenar_arreglo(){
+int arreglo[100000]; //Declaración de arreglo de forma global
+void llenar_arreglo(){ //Función void para llenar el arreglo de numeros aleatorios
   srand (time(NULL));
   for(int i=0; i<=100000;i++){
       int aleatorio = rand() % 10000+1;
@@ -11,7 +12,7 @@ void llenar_arreglo(){
   }
 }
 
-float suma(int arreglo[]){
+float suma(int arreglo[]){ //Función que recorre el arreglo sumando todos sus numeros aleatorios
   float suma;
   for(int i=0; i<=100000;i++){
     suma=suma+(arreglo[i]);
@@ -19,13 +20,24 @@ float suma(int arreglo[]){
   return suma;
 }
 
+float varianza (int arreglo[],float prom ){ // Función que calcula la varianza de los numeros dentro del arreglo
+  float var;
+  for(int i=0; i<=100000;i++){
+    var += pow(arreglo[i]-prom,2);
+    var=var/prom;
+  }
+}
+
 using namespace std;
 
 int main()
 {
-  float prom;
-  llenar_arreglo();
-  prom=(suma(arreglo)/p);
-  cout<<prom<<endl;
+  float prom,desv; // Declaración de variables promedio y desviación
+  llenar_arreglo(); // Se llama a la función void para llenar el arreglo
+  prom=(suma(arreglo)/100000); //Se calcula el promedio del arreglo
+  cout<<"El promedio es: "<<prom<<endl; //Salida por pantalla del promedio
+  cout<<"La varianza es: "<<varianza(arreglo,prom)<<endl; // Salida por pantalla de la varianza
+  desv=(sqrt(varianza(arreglo,prom))); // Se calcula la desviación utilizando la función de varianza
+  cout<<"La desviación estandar es: "<<desv<<endl; //Salida por pantalla de la desviación estandar
   return 0;
 }
